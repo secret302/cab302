bb2abe25e114f4b4bac8084ab2db87030df6ae97
Caleb Worth <calebworth03@gmail.com>
2024-04-25 09:23:32 +1000

    Basic View Switch Functionality Integrated

--- src/main/java/com/calendarfx/app/CalendarApp.java
+++ src/main/java/com/calendarfx/app/CalendarApp.java
@@ -21,6 +21,7 @@ import com.calendarfx.model.Calendar.Style;
 import com.calendarfx.model.CalendarSource;
 import com.calendarfx.view.AgendaView;
 import com.calendarfx.view.DetailedDayView;
+import com.calendarfx.view.DetailedWeekView;
 import com.calendarfx.view.YearMonthView;
 
 import fr.brouillard.oss.cssfx.CSSFX;
@@ -40,14 +41,17 @@ import javafx.stage.Stage;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
 public class CalendarApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        DetailedDayView calendarView = new DetailedDayView();
-        
-        calendarView.setEnableTimeZoneSupport(true);
-
+        DetailedDayView calendarDayView = new DetailedDayView();
+        DetailedWeekView calendarWeekView = new DetailedWeekView();
+        calendarDayView.setEnableTimeZoneSupport(true);
+        calendarWeekView.setEnableTimeZoneSupport(true);
         // Users can categories events to seperate work from life
         // This helps create work-life balance, thus mental wellbeing
         Calendar personal = new Calendar("Personal Events");
@@ -64,25 +68,31 @@ public class CalendarApp extends Application {
 
         CalendarSource mainCalendarSource = new CalendarSource("Main");
         mainCalendarSource.getCalendars().addAll(personal, study, work);
-        calendarView.getCalendarSources().setAll(mainCalendarSource);
-        calendarView.setRequestedTime(LocalTime.now());
-        calendarView.setMaxWidth(600);
+        calendarDayView.getCalendarSources().setAll(mainCalendarSource);
+        calendarDayView.setRequestedTime(LocalTime.now());
+        calendarDayView.setMaxWidth(600);
+        calendarWeekView.getCalendarSources().setAll(mainCalendarSource);
+        calendarWeekView.setRequestedTime(LocalTime.now());
+        calendarWeekView.setMaxWidth(1600);
+
+
 
         Rectangle switchViewBox = new Rectangle(100, 1000);
         switchViewBox.setFill(Color.GREY);
         Text switchViewText = new Text(">");
         StackPane switchViewButton = new StackPane();
         switchViewButton.getChildren().addAll(switchViewBox, switchViewText);
-
+        
         Text dateToday = new Text(LocalDate.now().toString());
         StackPane dateTodayPanel = new StackPane();
         dateTodayPanel.getChildren().addAll(dateToday);
         dateTodayPanel.setMaxWidth(600);
 
         VBox leftPanel = new VBox();
-        leftPanel.getChildren().addAll(dateTodayPanel, calendarView);
-        VBox.setVgrow(calendarView, Priority.ALWAYS);
-        leftPanel.setMaxWidth(600);
+        leftPanel.getChildren().addAll(dateTodayPanel, calendarDayView);
+        VBox.setVgrow(calendarDayView, Priority.ALWAYS);
+        leftPanel.prefWidth(600);
+    
 
         AgendaView agenda = new AgendaView();
         agenda.setEnableTimeZoneSupport(true);
@@ -106,13 +116,27 @@ public class CalendarApp extends Application {
         // Prevents Calendar from being squished by other HBox Components
         HBox.setHgrow(leftPanel, Priority.ALWAYS);
 
+        switchViewButton.setOnMouseClicked(event -> {
+            if (leftPanel.getChildren().contains(calendarDayView)){
+                leftPanel.getChildren().remove(calendarDayView);
+                leftPanel.getChildren().add(1, calendarWeekView);
+                VBox.setVgrow(calendarWeekView, Priority.ALWAYS);
+            }
+            else {
+                leftPanel.getChildren().remove(calendarWeekView);
+                leftPanel.getChildren().add(1, calendarDayView);
+                VBox.setVgrow(calendarDayView, Priority.ALWAYS);
+            }
+        });
+
+
         Thread updateTimeThread = new Thread("Calendar: Update Time Thread") {
             @Override
             public void run() {
                 while (true) {
                     Platform.runLater(() -> {
-                        calendarView.setToday(LocalDate.now());
-                        calendarView.setTime(LocalTime.now());
+                        calendarDayView.setToday(LocalDate.now());
+                        calendarDayView.setTime(LocalTime.now());
                     });
 
                     try {
