a344eceb469e2090da3eec0a8427a37ce76b8348
Scott May <129928587+Scotto808@users.noreply.github.com>
2024-05-10 21:37:26 +1000

    Completed JDOC comments for routineOneController and fixed few bugs. Incomplete; requires connection to calendar + goal/GoalDAO changes + Event changes.

--- src/main/java/com/serenitask/controller/RoutineOneController.java
+++ src/main/java/com/serenitask/controller/RoutineOneController.java
@@ -5,12 +5,10 @@ import com.calendarfx.model.Entry;
 import com.calendarfx.model.Interval;
 import com.serenitask.model.*;
 import com.serenitask.util.DatabaseManager.EventDAO;
-import com.serenitask.util.DatabaseManager.GoalDAO;
-import javafx.stage.Window;
+
 
 import java.time.Duration;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,9 +16,8 @@ import java.util.List;
 public class RoutineOneController {
 
     // Dummy value of 7, will be replaced with 28 for 1 month or ~4 weeks
-    private int allocationThreshold = 7;
-    private int blockSize = 7;
-    private LocalDate today = LocalDate.now();
+    private final int allocationThreshold = 7;
+    private final int blockSize = 7;
     private LocalTime DayStart = LocalTime.of(8, 0, 0);
     private LocalTime DayEnd = LocalTime.of(18, 0, 0);
 
@@ -31,10 +28,17 @@ public class RoutineOneController {
     DummyGoal goalTwo = new DummyGoal("Study", "null", 30, 120, 0, "never", "weekly");
     DummyGoal goalThree = new DummyGoal("Read", "null", 30, 60, 0, "never", "weekly");
 
+    /*
+    This routine currently operates on dummy values;
+    Routine requires updating pending changes to Event, Goal and GoalDAO
+    Event: requires constructor to create blank object.
+    goal and GoalDAO: Addition of an "AllocatedUpTo" value. Will store the date the optimizer has allocated up until.
+     */
 
-    // // Stages
-
-
+    /**
+     * Simple execution function that executes routine 1 of the optimizer.
+     * This routine handles the allocation of goals that require x time per repeating period y.
+     */
     public void runRoutine() {
         EventDAO eventDAO = new EventDAO();
         List<Event> eventList = eventDAO.getAllEvents();
@@ -48,6 +52,13 @@ public class RoutineOneController {
     }
 
     // 1. Pull list of time base-based goals
+
+    /**
+     * Extracts all goals from the database and returns them as a list. Goals extracted are of the type:
+     * requires x time per repeating period of y.
+     *
+     * @return List of all goals from the database
+     */
     private List<DummyGoal> getGoalList() {
         List<DummyGoal> goalList = new ArrayList<>();
         goalList.add(goalOne);
@@ -56,9 +67,16 @@ public class RoutineOneController {
         return goalList;
     }
 
-    ;
 
     // 2. Parse list and remove any that exceed the allocation data threshold today + ~4weeks
+
+    /**
+     * Parses the goal list and determines which goals require allocation. Goals allocated to or beyond the allocation
+     * threshold are not required to be allocated.
+     *
+     * @param goalList List of all goals requiring x time per y period
+     * @return List of goals requiring allocation
+     */
     private List<DummyGoal> parseGoalList(List<DummyGoal> goalList) {
         LocalDate targetDate = getTargetDate();
         List<DummyGoal> parsedList = new ArrayList<>();
@@ -76,40 +94,56 @@ public class RoutineOneController {
         return parsedList;
     }
 
-    ;
 
     // 3. Will now have a list of time based goals that need to be implemented
     // from (stored_date -> threshold_date)
     // use a ceiling type method, must allocate up until a sunday. so could be (4 weeks -- 4 weeks + 6 days)
     // allocate time based goals to fill windows
 
+    /**
+     * Allocates goals based on their required time per period. Accounts for partial and full allocation periods.
+     *
+     * @param goal      A goal object that requires calendar entry allocations
+     * @param eventList A list containing all events draw from the database to be parsed.
+     */
     private void allocateGoal(DummyGoal goal, List<Event> eventList) {
-
-        //LocalDate allocationStart = goal.getAllocatedUpTo().plusDays(1);
-
-        // Get the events list, Create a new list for each block,
-        // If start date on block start to block end
-        // then sort into days
-        // then prep next stage
 
         int firstBlock = getDifferenceSunday(goal.getAllocatedUpTo());
 
+        // TO DO !!!
         // This works, but now the entries returned via allocateBlock must be attached to calendar.
+        // TO DO !!!!
+
+        List<Entry<?>> entries = new ArrayList<>();
+
 
         if (firstBlock != blockSize) {
             int blockTarget = (int) Math.floor(goal.getGoalTargetAmount() * ((double) firstBlock / blockSize));
-            allocateBlock(blockTarget, goal, eventList);
+            entries = allocateBlock(blockTarget, goal, eventList);
             goal.subtractDaysOutstanding(firstBlock);
+            // Commit entries here
         }
 
         while (goal.getDaysOutstanding() > blockSize) {
             int blockTarget = goal.getGoalTargetAmount();
-            allocateBlock(blockTarget, goal, eventList);
+            entries = allocateBlock(blockTarget, goal, eventList);
             goal.subtractDaysOutstanding(blockSize);
+            // Commit entries here
         }
 
     }
 
+
+    /**
+     * Sophisticated allocation algorithm that takes in a blockTarget value, a goal and an eventList. Processes these
+     * given parameters to allocate period based goals into events and store them in the calendar. Used to generate
+     * period based goals: I want to do "goal" for x hours per y period.
+     *
+     * @param blockTarget int value representing minutes of time required to be allocated
+     * @param goal        current goal having time allocated
+     * @param eventList   raw list of events drawn from database
+     * @return returns a list of entries to be added to calendar
+     */
     private List<Entry<?>> allocateBlock(int blockTarget, DummyGoal goal, List<Event> eventList) {
         int buffer = 5;
         LocalDate allocationStart = goal.getAllocatedUpTo().plusDays(1);
@@ -155,6 +189,13 @@ public class RoutineOneController {
 
     }
 
+    /**
+     * Takes in a List of Lists containing events sorted by day. Uses events per day to evaluate available TimeWindows
+     * available for allocation. Each list of events is converted to a Day which contains a List of Windows.
+     *
+     * @param rawDaysLists List of lists containing events, sorted by date
+     * @return List of Day objects
+     */
     private List<Day> getDaysList(List<List<Event>> rawDaysLists) {
         List<Day> daysList = new ArrayList<>();
         for (List<Event> list : rawDaysLists) {
@@ -184,6 +225,13 @@ public class RoutineOneController {
     }
 
 
+    /**
+     * Takes in an unsorted List of day objects and sorts them based on the amount of unallocated time. returns a sorted
+     * list of days from most free time to least. Additionally, assigns priority to the day objects based on ordering.
+     *
+     * @param rawDays Raw day objects in list form. Unsorted, unprioritized.
+     * @return Sorted Day List, from most free time to least.
+     */
     private List<Day> prioritizeDays(List<Day> rawDays) {
 
         // sorting by free time;
@@ -212,6 +260,12 @@ public class RoutineOneController {
         return sorted;
     }
 
+    /**
+     * Takes in a single event list and sorts it based on start times. returns the list sorted in chronological order
+     *
+     * @param unsortedlist An unsorted list of event objects
+     * @return Chronologically sorted list of events
+     */
     private List<Event> getSortedList(List<Event> unsortedlist) {
         List<Event> sorted = new ArrayList<>();
 
@@ -231,6 +285,15 @@ public class RoutineOneController {
         return sorted;
     }
 
+    /**
+     * Function takes in a start and end date spanning a period of time. Parses the given eventList from start date
+     * to end date and creates a list of events per date. Returns a list of these day lists.
+     *
+     * @param start     LocalDate object representing the start date of the allocation period
+     * @param end       LocalDate object representing the end date of the allocation period
+     * @param eventList List object containing all given events.
+     * @return List of Lists. Splits all events into lists of lists
+     */
     private List<List<Event>> splitDays(LocalDate start, LocalDate end, List<Event> eventList) {
         List<List<Event>> dayLists = new ArrayList<>();
         while (end.compareTo(start) > 0) {
@@ -241,19 +304,33 @@ public class RoutineOneController {
                     dayList.add(event);
                 }
             }
+            start.plusDays(1);
             dayLists.add(dayList);
         }
 
         return dayLists;
     }
 
-    // Helpers
+
+    /**
+     * Gets the current date and returns a future date based on the allocationThreshold class parameter
+     * if current date is a sunday, will return date + (allocationThreshold) days
+     * if current date is not sunday, will return date + (allocationThreshold + n days) days
+     *
+     * @return new LocalDate used for target date
+     */
     private LocalDate getTargetDate() {
         LocalDate targetDate = LocalDate.now().plusDays(allocationThreshold);
 
         return getNextSunday(targetDate);
     }
 
+    /**
+     * Takes in a LocalDate value and returns the date of the nearest sunday.
+     *
+     * @param startDate any LocalDate object
+     * @return LocalDate of the closest following sunday
+     */
     private LocalDate getNextSunday(LocalDate startDate) {
         LocalDate sunday = startDate;
 
@@ -263,6 +340,13 @@ public class RoutineOneController {
         return sunday;
     }
 
+    /**
+     * Takes in a LocalDate value and returns the integer difference between the date given
+     * and the closest following sunday.
+     *
+     * @param date any LocalDate object
+     * @return int difference
+     */
     private int getDifferenceSunday(LocalDate date) {
         LocalDate targetDate = date;
 
