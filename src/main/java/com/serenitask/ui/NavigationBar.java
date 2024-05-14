9cb70cfd9cffa0206cd4a3a337a465b0cfbeff6f
Scott May <129928587+Scotto808@users.noreply.github.com>
2024-05-14 21:47:18 +1000

    NavigationBar Class finished and functional

--- src/main/java/com/serenitask/ui/NavigationBar.java
+++ src/main/java/com/serenitask/ui/NavigationBar.java
@@ -1,35 +1,50 @@
 package com.serenitask.ui;
 
 import com.calendarfx.view.DateControl;
+import com.calendarfx.view.DetailedDayView;
+import com.calendarfx.view.DetailedWeekView;
 import com.calendarfx.view.Messages;
 import impl.com.calendarfx.view.NavigateDateView;
 import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+
+import java.time.LocalDate;
 
 public class NavigationBar extends DateControl {
 
 private final BooleanProperty showNavigation = new SimpleBooleanProperty(this, "showNavigation", true);
 
+
 public final BooleanProperty showNavigationProperty() {
     return showNavigation;}
 
-    public NavigateDateView createButton()
+    public NavigateDateView createButton(DetailedDayView calendarDayView, DetailedWeekView calendarWeekView)
     {
         NavigateDateView navBar = new NavigateDateView();
-        navBar.getTodayButton().setText(Messages.getString("Today"));
-        navBar.setOnBackward(() -> goBack());
-        navBar.setOnToday(() -> goToday());
-        navBar.setOnForward(() -> goForward());
+        calendarWeekView.setDate(LocalDate.now());
+        calendarDayView.setDate(LocalDate.now());
+        navBar.setOnBackward(() -> {
+            calendarDayView.setDate(calendarDayView.getDate().minusDays(1));
+            calendarWeekView.setDate(calendarWeekView.getStartDate().minusWeeks(1));
+        });
+        navBar.setOnForward(() -> {
+            calendarDayView.setDate(calendarDayView.getDate().plusDays(1));
+            calendarWeekView.setDate(calendarWeekView.getStartDate().plusWeeks(1));
+        });
+        navBar.setOnToday(() -> {
+            calendarDayView.setDate(LocalDate.now());
+            calendarWeekView.setDate(LocalDate.now());
+        });
         navBar.visibleProperty().bind(showNavigationProperty());
         return navBar;
     }
 
-
     public void goBack() {
         setDate(getDate().minusDays(1));
     }
-
-
 
     public void goToday() {
         setDate(getToday());
@@ -38,6 +53,7 @@ public final BooleanProperty showNavigationProperty() {
     public void goForward() {
         setDate(getDate().plusDays(1));
     }
+
 
 
 }
