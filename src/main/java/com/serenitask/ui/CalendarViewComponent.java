3e7e9e97e85f1d550002d443d26afd1d0f131f05
Scott May <129928587+Scotto808@users.noreply.github.com>
2024-05-14 23:14:36 +1000

    Fixed imports

--- src/main/java/com/serenitask/ui/CalendarViewComponent.java
+++ src/main/java/com/serenitask/ui/CalendarViewComponent.java
@@ -1,16 +1,11 @@
 package com.serenitask.ui;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.calendarfx.view.DetailedDayView;
 import com.calendarfx.view.DetailedWeekView;
-import com.serenitask.controller.GoalController;
 
 import impl.com.calendarfx.view.NavigateDateView;
-import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
@@ -18,8 +13,6 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
@@ -31,11 +24,12 @@ public class CalendarViewComponent {
     /**
      * Configures the main display for showing the current date on the calendar view.
      *
-     * @param dateToday      A Text object to display the current date.
-     * @param dateTodayPanel An HBox that holds the date display.
-     * @param spacer         A Region used to manage spacing within the layout.
+     * @param dateToday        A Text object to display the current date.
+     * @param dateTodayPanel   An HBox that holds the date display.
+     * @param spacer           A Region used to manage spacing within the layout.
+     * @param navigateDateView
      */
-    public static void calendarView(Text dateToday, HBox dateTodayPanel, Region spacer) {
+    public static void calendarView(Text dateToday, HBox dateTodayPanel, Region spacer, NavigateDateView navigateDateView) {
         dateToday.setFont(Font.font(40));
 
         dateTodayPanel.getChildren().addAll(dateToday, spacer);
