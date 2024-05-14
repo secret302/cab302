e60eeeb6c4b6eb436e38ebcbf7a5db87ad4aaf08
lucakralj <lucakralj@protonmail.com>
2024-05-14 18:00:02 +1000

    added functionality to AddGoal component. Component now opens when clicked, and can submit an entry to the database. Functionality is not yet fully complete.

--- src/main/java/com/serenitask/ui/WindowComponents/AddGoal.java
+++ src/main/java/com/serenitask/ui/WindowComponents/AddGoal.java
@@ -1,42 +1,22 @@
 package com.serenitask.ui.WindowComponents;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.IntStream;
 
-import com.calendarfx.view.AgendaView;
-import com.calendarfx.view.DetailedDayView;
-import com.calendarfx.view.DetailedWeekView;
-
-
-import com.serenitask.controller.GoalController;
-import javafx.application.Platform;
-import javafx.geometry.Insets;
+import com.serenitask.model.Goal;
+import com.serenitask.util.DatabaseManager.GoalDAO;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 
 public class AddGoal {
     public static void displayAddGoalView() {
         Stage popOutStage = new Stage();
         popOutStage.initModality(Modality.APPLICATION_MODAL);
         popOutStage.setTitle("Add Goal");
-
         Label wantLabel = new Label("I want to do:");
         TextField wantInput = new TextField();
 
@@ -47,18 +27,50 @@ public class AddGoal {
         TextField minuteInput = new TextField();
         Label minuteLabel = new Label("minutes.");
         Label perLabel = new Label("Per");
+
+        Label minChunkLabel = new Label("Min chunk:");
+        ComboBox<Integer> minChunkInput = new ComboBox<>();
+        minChunkInput.getItems().addAll(15, 30, 45, 60);
+
+        Label maxChunkLabel = new Label("Max chunk:");
+        ComboBox<Integer> maxChunkInput = new ComboBox<>();
+        maxChunkInput.getItems().addAll(15, 30, 45, 60);
+
+        Label goalEndDateLabel = new Label("Enter the date you wish to complete your goal by:");
+        DatePicker goalEndDateInput = new DatePicker(LocalDate.now());
+
         ComboBox<String> periodInput = new ComboBox<>();
-        // PLACEHOLDER VALUES?
+        // REFACTOR THIS
         periodInput.getItems().addAll("Day", "Week", "Month", "Year");
 
         Button saveButton = new Button("Save");
-        saveButton.setOnAction(e -> popOutStage.close());
+        saveButton.setOnAction(e -> {
+            String title = wantInput.getText();
+            LocalDate goalEndDate = goalEndDateInput.getValue();
+
+            Integer minChunkValue = minChunkInput.getValue();
+            Integer maxChunkValue = maxChunkInput.getValue();
+
+            if (minChunkValue == null || maxChunkValue == null) {
+                System.err.println("Please select values for minChunk and maxChunk.");
+                return;
+            }
+
+            int minChunk = minChunkInput.getValue();
+            int maxChunk = maxChunkInput.getValue();
+
+            Goal goal = new Goal(title, minChunk, maxChunk);
+            GoalDAO goalDAO = new GoalDAO();
+            goalDAO.addGoal(goal);
+
+            popOutStage.close();
+                });
         Button backButton = new Button("Back");
         backButton.setOnAction(e -> popOutStage.close());
 
         VBox layout = new VBox(10);
         layout.getChildren().addAll(wantLabel, wantInput, forLabel, hourInput, hourLabel,
-                andLabel, minuteInput, minuteLabel, perLabel, periodInput, backButton, saveButton);
+                andLabel, minuteInput, minuteLabel, perLabel, periodInput, minChunkLabel, minChunkInput, maxChunkLabel, maxChunkInput, backButton, saveButton);
         layout.setAlignment(Pos.BASELINE_LEFT);
         Scene popOutScene = new Scene(layout, 500, 500);
         popOutStage.setScene(popOutScene);
