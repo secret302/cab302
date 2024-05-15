8005149e7092011c3f4cca3c815a39553b125401
Scott May <129928587+Scotto808@users.noreply.github.com>
2024-05-15 14:52:46 +1000

    Added JavaDoc comments and adjusted openOptimiserMenu

--- src/main/java/com/serenitask/controller/ShortcutController.java
+++ src/main/java/com/serenitask/controller/ShortcutController.java
@@ -12,7 +12,15 @@ import com.serenitask.model.Optimiser;
 
 import java.time.LocalTime;
 
+/**
+ * Creates listeners and attaches them to a scene. Implements shortcuts into the scene and their actions when triggered
+ */
 public class ShortcutController {
+    /**
+     * Creates listeners to handle key press events and attaches them to the scene.
+     * @param scene Current scene contained in Stage of javaFx
+     * @param mainCalendarSource Object containing all calendars
+     */
     public static void setupShortcuts(Scene scene, CalendarSource mainCalendarSource) {
 
         KeyCombination goalCombo = new KeyCodeCombination(KeyCode.G, KeyCombination.CONTROL_DOWN);
@@ -35,15 +43,27 @@ public class ShortcutController {
         });
     }
 
+    /**
+     * Shortcut event action that opens the Add Goal menu
+     */
     private static void openGoalAddMenu() {
         AddGoal.displayAddGoalView();
     }
 
+    /**
+     * Shortcut event action that opens the Add Event menu
+     * @param mainCalendarSource Calendarsource containing calendars to add event
+     */
     private static void openEventAddMenu(CalendarSource mainCalendarSource) {
         AddEvent.displayAddEventView(mainCalendarSource);
     }
 
+    /**
+     * Shortcut event action that runs the optimiser
+     * @param calendarSource CalendarSource containing the calendars to be optimised
+     */
     private static void openOptimiserMenu(CalendarSource calendarSource) {
+        // Gross duplicate dummy values in 2 areas. Make a database table or object to hold this.
         LocalTime userDayStart = LocalTime.of(8, 0, 0);
         LocalTime userDayEnd = LocalTime.of(18, 30, 0);
         int allocateAhead = 7;
