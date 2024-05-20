a0db0f8634487f93fea784fdbd3ebf0d5057e931
CAPT-Zulu <thebox@outlook.com.au>
2024-05-20 22:41:54 +1000

    Refactored SqliteConnection to follow new standards

--- src/main/java/com/serenitask/util/DatabaseManager/SqliteConnection.java
+++ src/main/java/com/serenitask/util/DatabaseManager/SqliteConnection.java
@@ -1,44 +1,49 @@
 package com.serenitask.util.DatabaseManager;
 
+import com.serenitask.util.ErrorHandler;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * SqliteConnection class creates a connection to the SQLite database
- * Singleton pattern is used to ensure only one connection is created
+ * SqliteConnection provides a singleton instance of the database connection.
+ * Ensures that only one connection to the SQLite database is established throughout the application.
  */
 public class SqliteConnection {
-    // Singleton instance
-    private static Connection instance = null;
 
+    /** The singleton instance of the database connection. */
+    private static Connection instance;
 
     /**
-     * SqliteConnection constructor
-     * Creates a connection to the SQLite database
+     * Private constructor to prevent external instantiation.
+     * Initializes the connection to the SQLite database when the class is loaded.
      */
     private SqliteConnection() {
         // SQLite connection URL
         String url = "jdbc:sqlite:database.db";
+
+        // Attempt to establish a connection to the database
         try {
-            // Attempt to create a new connection to the SQLite database
             instance = DriverManager.getConnection(url);
-        } catch (SQLException sqlEx) {
-            // Print the error message
-            System.err.println(sqlEx);
+        } catch (SQLException e) {
+            // Handle SQL errors
+            ErrorHandler.handleException(e);
         }
     }
 
     /**
-     * Get the singleton connection instance to the SQLite database
-     * @return Connection object
+     * Provides the singleton instance of the database connection.
+     * If the connection doesn't exist, it initializes a new connection.
+     *
+     * @return The Connection object representing the connection to the database.
      */
     public static Connection getConnection() {
+        // Create a new connection if it doesn't exist
         if (instance == null) {
-            // Create a new connection if one does not exist
             new SqliteConnection();
         }
-        // Return the connection instance
+        // Return the existing connection
         return instance;
     }
 }
