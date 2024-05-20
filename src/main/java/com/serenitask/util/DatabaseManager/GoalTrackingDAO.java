706e9394c09d6b658d06fa8ea6950fe0714c553a
CAPT-Zulu <thebox@outlook.com.au>
2024-05-20 22:38:30 +1000

    Refactored GoalTrackingDAO Part 2 (Final one I swear)

--- src/main/java/com/serenitask/util/DatabaseManager/GoalTrackingDAO.java
+++ src/main/java/com/serenitask/util/DatabaseManager/GoalTrackingDAO.java
@@ -4,6 +4,7 @@ import com.serenitask.model.GoalTracking;
 import com.serenitask.util.ErrorHandler;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -59,7 +60,7 @@ public class GoalTrackingDAO {
             PreparedStatement statement = connection.prepareStatement(query);
             // Update new row with values from goalTracking
             statement.setInt(1, goalTracking.getId());
-            statement.setString(2, goalTracking.getGoalDate());
+            statement.setDate(2, java.sql.Date.valueOf(goalTracking.getGoalDate()));
             statement.setBoolean(3, goalTracking.getCompleted());
 
             // Execute update
@@ -90,7 +91,7 @@ public class GoalTrackingDAO {
             PreparedStatement statement = connection.prepareStatement(query);
             // Update row with values from Goal Tracking
             statement.setInt(1, goalTracking.getId());
-            statement.setString(2, goalTracking.getGoalDate());
+            statement.setDate(2, java.sql.Date.valueOf(goalTracking.getGoalDate());
             statement.setBoolean(3, goalTracking.getCompleted());
             // Execute update
             statement.executeUpdate();
@@ -110,15 +111,15 @@ public class GoalTrackingDAO {
      * @param id Goal Tracking ID
      * @param date Goal Tracking Date
      * @return True if the Goal Tracking entry was deleted successfully
-     *
-    public boolean deleteGoalTracking(int id, string date) {
+     */
+    public boolean deleteGoalTracking(int id, LocalDate date) {
         try {
             // Create delete query
             String query = "DELETE FROM goal_tracking WHERE id = ? AND goal_date = ?";
             PreparedStatement statement = connection.prepareStatement(query);
             // Delete row id
             statement.setInt(1, id);
-            statement.setInt(1, date);
+            statement.setDate(2, java.sql.Date.valueOf(date));
             // Execute update
             statement.executeUpdate();
 
@@ -130,7 +131,6 @@ public class GoalTrackingDAO {
         }
         return false;
     }
-     */
 
     /**
      * Retrieves a GoalTracking record from the database by its goal ID and date.
@@ -139,19 +139,18 @@ public class GoalTrackingDAO {
      * @param date  The date for which tracking information is retrieved.
      * @return The GoalTracking object representing the tracking information, or null if not found.
      */
-    public GoalTracking getGoalTrackingByIdAndDate(int goalId, String date) {
+    public GoalTracking getGoalTrackingByIdAndDate(int goalId, LocalDate date) {
         try {
             // Select the goal tracking
             PreparedStatement statement = connection.prepareStatement("SELECT * FROM goal_tracking WHERE id = ? AND goal_date =?");
             statement.setInt(1, goalId);
-            statement.setString(2, date);
+            statement.setDate(2, java.sql.Date.valueOf(date));
             ResultSet resultSet = statement.executeQuery();
             // If goal returned, return constructed
             if (resultSet.next()) {
                 return constructGoalTrackingFromResultSet(resultSet);
             }
         } catch (SQLException e) {
-
             // Handle SQL errors
             ErrorHandler.handleException(e);
         }
@@ -166,20 +165,27 @@ public class GoalTrackingDAO {
      * @return A list of GoalTracking objects representing the tracking information for the specified goal.
      */
     public List<GoalTracking> getGoalTrackingByGoalId(int goalId) {
+        // Create empty list
         List<GoalTracking> goalTrackingList = new ArrayList<>();
-        String query = "SELECT * FROM goal_tracking WHERE goal_id = ?";
-
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
+        try {
+            // Create query
+            String query = "SELECT * FROM goal_tracking WHERE goal_id = ?";
+            // Create prepared statement
+            PreparedStatement statement = connection.prepareStatement(query);
+            // Insert goal ID into query
             statement.setInt(1, goalId);
+            // Execute query
+            ResultSet resultSet = statement.executeQuery();
 
-            try (ResultSet resultSet = statement.executeQuery()) {
-                while (resultSet.next()) {
-                    goalTrackingList.add(constructGoalTrackingFromResultSet(resultSet));
-                }
+            // Add all results to list
+            while (resultSet.next()) {
+                goalTrackingList.add(constructGoalTrackingFromResultSet(resultSet));
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            // Handle SQL errors
+            ErrorHandler.handleException(e);
         }
+        // Return list even if empty
         return goalTrackingList;
     }
 
@@ -190,20 +196,29 @@ public class GoalTrackingDAO {
      * @return A list of GoalTracking objects representing the tracking information for the specified date.
      */
     public List<GoalTracking> getGoalTrackingByDate(String date) {
+        // Create empty list
         List<GoalTracking> goalTrackingList = new ArrayList<>();
-        String query = "SELECT * FROM goal_tracking WHERE goal_date = ?";
 
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
+        try{
+            // Create query
+            String query = "SELECT * FROM goal_tracking WHERE goal_date = ?";
+            // Create prepared statement
+            PreparedStatement statement = connection.prepareStatement(query);
+            // Insert date into query
             statement.setString(1, date);
 
+            // Execute query
             try (ResultSet resultSet = statement.executeQuery()) {
+                // Add all results to list
                 while (resultSet.next()) {
                     goalTrackingList.add(constructGoalTrackingFromResultSet(resultSet));
                 }
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            // Handle SQL errors
+            ErrorHandler.handleException(e);
         }
+        // Return list even if empty
         return goalTrackingList;
     }
 
@@ -213,17 +228,25 @@ public class GoalTrackingDAO {
      * @return A list of GoalTracking objects representing all goal tracking information stored in the database.
      */
     public List<GoalTracking> getAllGoalTrackingRecords() {
+        // Create query
         List<GoalTracking> goalTrackingList = new ArrayList<>();
-        String query = "SELECT * FROM goal_tracking";
 
-        try (Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(query)) {
+        try {
+            String query = "SELECT * FROM goal_tracking";
+            // Create statement
+            Statement statement = connection.createStatement();
+            // Execute query
+            ResultSet resultSet = statement.executeQuery(query);
+
+            // Add all results to list
             while (resultSet.next()) {
                 goalTrackingList.add(constructGoalTrackingFromResultSet(resultSet));
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            // Handle SQL errors
+            ErrorHandler.handleException(e);
         }
+        // Return list even if empty
         return goalTrackingList;
     }
 
@@ -242,3 +265,112 @@ public class GoalTrackingDAO {
         return new GoalTracking(goalId, goalDate, completed);
     }
 }
+
+/* Original Code, kept in case of requiring fazed out methods
+private List<Integer> getIdList(List<GoalTracking> unsorted)
+{
+    List<Integer> goalIds = new ArrayList<>();
+    for (GoalTracking goal : unsorted)
+    {
+        int id = goal.getId();
+        if(goalIds.contains(id) == false)
+        {
+            goalIds.add(id);
+        }
+    }
+    return goalIds;
+}
+
+private List<List<GoalTracking>> sortTrackedGoals(List<GoalTracking> unsorted)
+{
+    // Setup return list and list of unique IDs
+    List<List<GoalTracking>> AllGoals = new ArrayList<>();
+    List<Integer> IdList = getIdList(unsorted);
+
+    // For each unique ID, add all entries to a list and append to the AllGoals List
+    for (Integer ID : IdList)
+    {
+        List<GoalTracking> goalTracked = new ArrayList<>();
+
+        for (GoalTracking goal : unsorted)
+        {
+            if(ID == goal.getId())
+            {
+                goalTracked.add(goal);
+            }
+        }
+
+        AllGoals.add(goalTracked);
+    }
+
+    // Return list of lists
+    return AllGoals;
+}
+
+public List<GoalTracking> getTrackedGoalById(int id) {
+    List<GoalTracking> goals = new ArrayList<>();
+    try {
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM goal_tracking WHERE id = ? ");
+        statement.setInt(1, id);
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            // Retrieve data from the result set
+            int goal_id = resultSet.getInt("goal_id");
+            String goal_date = resultSet.getString("goal_date");
+            Boolean completed = resultSet.getBoolean("completed");
+            // Create a new event object
+            GoalTracking goalTracking = new GoalTracking(goal_id, goal_date, completed);
+            goals.add(goalTracking);
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return goals;
+}
+
+public List<GoalTracking> getTrackedGoalByDate(String date) {
+    List<GoalTracking> goals = new ArrayList<>();
+    try {
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM goal_tracking WHERE goal_date = ? ");
+        statement.setString(1, date);
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            // Retrieve data from the result set
+            int goal_id = resultSet.getInt("goal_id");
+            String goal_date = resultSet.getString("goal_date");
+            Boolean completed = resultSet.getBoolean("completed");
+            // Create a new event object
+            GoalTracking goalTracking = new GoalTracking(goal_id, goal_date, completed);
+            goals.add(goalTracking);
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return goals;
+}
+
+public List<List<GoalTracking>> getAllTrackedGoals() {
+    List<List<GoalTracking>> AllGoals = new ArrayList<>();
+    List<GoalTracking> unsorted = new ArrayList<>();
+    try {
+        Statement statement = connection.createStatement();
+        String query = "SELECT * FROM goal_tracking";
+        ResultSet resultSet = statement.executeQuery(query);
+        while (resultSet.next()) {
+            // Retrieve data from the result set
+            int goal_id = resultSet.getInt("goal_id");
+            String goal_date = resultSet.getString("goal_date");
+            Boolean completed = resultSet.getBoolean("completed");
+            // Create a new event object
+            GoalTracking goalTracking = new GoalTracking(goal_id, goal_date, completed);
+            unsorted.add(goalTracking);
+        }
+        AllGoals = sortTrackedGoals(unsorted);
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return AllGoals;
+}
+*/
