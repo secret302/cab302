1951b5e421856bc2de0a7766a7d583b9f64450e8
CAPT-Zulu <thebox@outlook.com.au>
2024-05-28 14:58:13 +1000

    Updated SettingsComp with new SettingsController

--- src/main/java/com/serenitask/ui/WindowComponents/SettingsComp.java
+++ src/main/java/com/serenitask/ui/WindowComponents/SettingsComp.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.serenitask.controller.SettingsController;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
@@ -12,6 +13,9 @@ import javafx.scene.text.Font;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import static com.serenitask.controller.SettingsController.addSetting;
+import static com.serenitask.controller.SettingsController.saveSettings;
 
 
 /**
@@ -24,39 +28,27 @@ public class SettingsComp {
      * Displays a modal window for settings, allowing the user to input settings.
      * The window contains form inputs for the day start, end, allocate ahead, select date, and select calendar.
      */
+
     public static void displaySettingsView() {
         Stage popOutStage = new Stage();
         popOutStage.initModality(Modality.APPLICATION_MODAL);
         popOutStage.setTitle("Settings");
 
+        VBox layout = new VBox(20);
+        // HOw do I do padding here?
 
+        // Add application settings to the dialog
+        addSetting("Day Start", "time", "09:00", layout);
+        addSetting("Day End", "time", "18:00", layout);
+        addSetting("Allocate Ahead (Days)", "number", "7", layout);
+        addSetting("Selected Calendar", "choice", "Personal,Work,Goals", layout);
+        addSetting("Enable Notifications", "boolean", "true", layout);
 
-        Label startTimeLabel = new Label("Day Start:");
-        ComboBox<String> startTimeInput = createTimeComboBox();
-        startTimeLabel.setFont(Font.font(30));
-
-        Label endTimeLabel = new Label("Day End:");
-        ComboBox<String> endTimeInput = createTimeComboBox();
-        endTimeLabel.setFont(Font.font(30));
-
-        Label startDateLabel = new Label("Allocate Ahead:");
-        DatePicker startDateInput = new DatePicker(LocalDate.now());
-        startDateInput.setMaxWidth(110);
-        startDateLabel.setFont(Font.font(30));
-
-        Label calendarSelectLabel = new Label("Select Calendar");
-        calendarSelectLabel.setFont(Font.font(30));
-        ComboBox<String> calendarSelectInput = new ComboBox<>();
-        calendarSelectInput.setMaxWidth(150);
-        // PLACEHOLDER CALENDARS
-        calendarSelectInput.getItems().addAll("Personal Events", "Health", "Goals");
-
+        // Buttons for saving and discarding changes.
         Button saveButton = new Button("Save");
         saveButton.setStyle("-fx-background-color: lightgreen; -fx-font-size: 20");
         saveButton.setOnAction(e -> {
-
-            // insert things to save
-
+            saveSettings(); // Save the settings to the database
             popOutStage.close();
         });
 
@@ -67,30 +59,7 @@ public class SettingsComp {
         HBox buttonBox = new HBox(10);
         buttonBox.getChildren().addAll(backButton, saveButton);
         buttonBox.setAlignment(Pos.CENTER);
-
-        VBox startDateVBox = new VBox(15);
-        startDateVBox.getChildren().addAll(startDateLabel, startDateInput);
-        startDateVBox.setAlignment(Pos.CENTER);
-
-        VBox startTimeVBox = new VBox(15);
-        startTimeVBox.getChildren().addAll(startTimeLabel, startTimeInput);
-        startTimeVBox.setAlignment(Pos.CENTER);
-        VBox endTimeVBox = new VBox(15);
-        endTimeVBox.getChildren().addAll(endTimeLabel, endTimeInput);
-        endTimeVBox.setAlignment(Pos.CENTER);
-
-        HBox dateHBox = new HBox(20);
-        dateHBox.getChildren().addAll(startDateVBox);
-        dateHBox.setAlignment(Pos.CENTER);
-    
-        VBox timeVBox = new VBox(20);
-        timeVBox.getChildren().addAll(startTimeVBox, endTimeVBox);
-        timeVBox.setAlignment(Pos.CENTER);
-
-        VBox layout = new VBox(20);
-        layout.getChildren().addAll(dateHBox, timeVBox,
-                calendarSelectLabel, calendarSelectInput, buttonBox);
-        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().add(buttonBox);
 
         Scene popOutScene = new Scene(layout, 500, 550);
         popOutStage.setScene(popOutScene);
